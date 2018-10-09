e144031aa4c2f1435225bd0c8c080de2321bf633
Chris Webb <chris.webb@ACCESS-0M8F8JC.uk.access-accounts.com>
2018-10-09 11:23:06 +0100

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,20 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def download_qt
+    send_file(
+      "#{Rails.root}/public/qt@5.5.zip",
+      filename: "qt@5.5.zip",
+      type: "application/zip"
+    )
+  end
+
+  def download_mysql
+    send_file(
+      "#{Rails.root}/public/mysql.zip",
+      filename: "mysql.zip",
+      type: "application/zip"
+    )
+  end
+end
